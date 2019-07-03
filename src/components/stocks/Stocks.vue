c342fd8e3a37b55ca49f894c67c3f6eb45e5e0b9
John Lu <johnluchn@gmail.com>
2019-07-03 11:43:07 +0800

    创建股票组件

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,5 +1,25 @@
 <template>
   <div>
-    <h1>The Stocks Component</h1>
+    <app-stock v-for="(stock, index) in stocks" :key="index"></app-stock>
   </div>
 </template>
+
+<script>
+import appStock from './Stock'
+
+export default {
+  data() {
+    return {
+      stocks: [
+        { id: 1, name: 'BMW', price: 110 },
+        { id: 2, name: 'Facebook', price: 130 },
+        { id: 3, name: 'Microsoft', price: 310 },
+        { id: 4, name: 'Google', price: 210 },
+      ]
+    }
+  },
+  components: {
+    appStock
+  }
+}
+</script>
