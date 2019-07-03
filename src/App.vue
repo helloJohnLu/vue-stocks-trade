895f42e32d1e5cdf1d0c2306694fa87db313e1bf
John Lu <johnluchn@gmail.com>
2019-07-03 10:41:21 +0800

    配置路由

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="container">
-    
+    <ul>
+      <li>
+        <router-link tag="li" to="/"><a>Home</a></router-link>
+        <router-link tag="li" to="/portfolio"><a>Portfolio</a></router-link>
+        <router-link tag="li" to="/stocks"><a>Stocks</a></router-link>
+      </li>
+    </ul>
+    <hr>
+    <router-view></router-view>
   </div>
 </template>
 
