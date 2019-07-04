eb3714d56cdf202b675011079b059b23528df997
John Lu <johnluchn@gmail.com>
2019-07-04 18:02:50 +0800

    股票交易——卖出数量不能超过自己持有的

--- src/components/portfolio/Stock.vue
+++ src/components/portfolio/Stock.vue
@@ -14,14 +14,15 @@
             class="form-control"
             placeholder="Quantity"
             v-model.number="quantity"
+            :class=" { danger: insufficientQuantity } "
           >
         </div>
         <div class="pull-right">
           <button 
             class="btn btn-info"
             @click="sellStock"
-            :disabled="quantity <= 0 || !Number.isInteger(quantity)"
-          >Sell</button>
+            :disabled="insufficientQuantity || quantity <= 0 || !Number.isInteger(quantity)"
+          > {{ insufficientQuantity ? 'Not enough' : 'Sell' }} </button>
         </div>
       </div>
     </div>
@@ -34,6 +35,11 @@ export default {
   data () {
     return {
       quantity: 0
+    }
+  },
+  computed: {
+    insufficientQuantity() {
+      return this.quantity > this.stock.quantity;
     }
   },
   methods: {
@@ -49,3 +55,9 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+  .danger {
+    border: 1px solid red;
+  }
+</style>
