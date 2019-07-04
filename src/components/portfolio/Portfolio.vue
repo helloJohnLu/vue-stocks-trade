645b906c7acb553d75917b9666576e1fbab4c675
John Lu <johnluchn@gmail.com>
2019-07-04 17:03:10 +0800

    交易股票功能实现

--- src/components/portfolio/Portfolio.vue
+++ src/components/portfolio/Portfolio.vue
@@ -1,5 +1,24 @@
 <template>
   <div>
-    <h1>The Portfolio Component</h1>
+    <app-stock
+      v-for="(stock, index) in stocks" 
+      :key="index"
+      :stock="stock"
+    ></app-stock>
   </div>
 </template>
+
+<script>
+import Stock from './Stock'
+
+export default {
+  components: {
+    appStock: Stock
+  },
+  computed: {
+    stocks () {
+      return this.$store.getters.stockPortfolio;
+    }
+  }
+}
+</script>
