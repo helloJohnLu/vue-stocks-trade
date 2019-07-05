7eaa21bf00a4781b072848dddc862c082fd27476
John Lu <johnluchn@gmail.com>
2019-07-05 11:53:17 +0800

    获取数据（GET）

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -27,7 +27,7 @@
               aria-expanded="false">Save & Load <span class="caret"></span></a>
             <ul class="dropdown-menu">
               <li><a href="#" @click="saveData">Save Data</a></li>
-              <li><a href="#">Load Data</a></li>
+              <li><a href="#" @click="loadData">Load Data</a></li>
             </ul>
           </li>
         </ul>
@@ -51,9 +51,10 @@ export default {
     }
   },
   methods: {
-    ...mapActions([
-      'randomizeStocks',
-    ]),
+    ...mapActions({
+      randomizeStocks: 'randomizeStocks',
+      fetchData: 'loadData'
+    }),
     endDay() {
       this.randomizeStocks();
     },
@@ -64,6 +65,9 @@ export default {
         stocks: this.$store.getters.stocks
       };
       this.$http.put('data.json', data);
+    },
+    loadData() {
+      this.fetchData();
     }
   },
 }
