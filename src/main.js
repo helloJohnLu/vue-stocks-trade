06b3cef965313dce1144744ad7884c4e400ae542
John Lu <johnluchn@gmail.com>
2019-07-05 10:30:17 +0800

    安装vue-resource，配置Firebase数据库

--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@ import App from './App.vue'
 import VueRouter from 'vue-router'
 import { routes } from "./routes";
 import { store } from "./store/store";
+import VueResource from 'vue-resource'
 
 Vue.use(VueRouter);
+Vue.use(VueResource);
+
+// firebase 数据库根链接
+Vue.http.options.root = 'https://vue-stocks-trade-19926.firebaseio.com/';
 
 Vue.filter('currency', (value) => {
   return '$' + value.toLocaleString();
